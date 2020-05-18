9d4942636fb539a4d77c1b2246a90fb75c7d076c
Mioksi <mio.natali@hotmail.com>
2020-05-18 18:05:17 +0300

    Добавляет в provider проверку на доступность интернета

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,21 +1,41 @@
+const isOnline = () => {
+  return window.navigator.onLine;
+};
+
 export default class Provider {
   constructor(api) {
     this._api = api;
   }
 
   getTasks() {
-    return this._api.getTasks();
+    if (isOnline()) {
+      return this._api.getTasks();
+    }
+
+    return Promise.reject(`offline logic is not implemented`);
   }
 
   createTask(task) {
-    return this._api.createTask(task);
+    if (isOnline()) {
+      return this._api.createTask(task);
+    }
+
+    return Promise.reject(`offline logic is not implemented`);
   }
 
   updateTask(id, task) {
-    return this._api.updateTask(id, task);
+    if (isOnline()) {
+      return this._api.updateTask(id, task);
+    }
+    
+    return Promise.reject(`offline logic is not implemented`);
   }
 
   deleteTask(id) {
-    return this._api.deleteTask(id);
+    if (isOnline()) {
+      return this._api.deleteTask(id);
+    }
+
+    return Promise.reject(`offline logic is not implemented`);
   }
 }
